c7c3c1658de2561a781629e471d00e038c85bf37
Carlos Mathiasen <carlos@blueguazu.com>
2018-03-27 10:20:05 -0300

    Ex2

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -26,6 +26,10 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
+mu = mean(X_norm);
+sigma = std(X_norm);
+
+X_norm = (X - mu) ./ sigma;
 
 
 
